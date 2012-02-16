619eeaad79897eff2ef67c3e8e981890a3c3e86f
Oto Brglez <otobrglez@gmail.com>
2012-02-16 13:49:16 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radddar::Application.config.secret_token = 'df82452fddb76b878cbbcedf3eb9837cb6474010dd44b498dc4a3be6579af690fea25a03f16bc5ccd6db5274bdea1e3fc22dce947d8490f6b3d6d4e556755079'
