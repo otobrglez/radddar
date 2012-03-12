c0ba4aacb12d51fc3920cbd76f64c7355eb8c206
Oto Brglez <otobrglez@gmail.com>
2012-03-12 23:23:45 +0100

    Pusher configuration changes

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,6 +3,8 @@ require "pusher"
 
 PUSHER = YAML.load_file("#{Rails.root.to_s}/config/pusher.yml")[Rails.env]
 
-Pusher.app_id = PUSHER["app_id"]
-Pusher.key = PUSHER["key"]
-Pusher.secret = PUSHER["secret"]
+if Rails.env!="production"
+	Pusher.app_id = PUSHER["app_id"]
+	Pusher.key = PUSHER["key"]
+	Pusher.secret = PUSHER["secret"]
+end
