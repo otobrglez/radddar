6099d5fbe737452b7fb6be62c9e84d3eaa464599
Oto Brglez <otobrglez@gmail.com>
2012-05-16 18:00:59 +0200

    Update to age problem

--- hang_test.sh
+++ hang_test.sh
@@ -1,3 +1,3 @@
-curl -F 'access_token=AAAFRcZBs57AcBAIClzz2Hjv6seHDWSIvIYyE2pJOcOQeO5ccQ31bVdZBTszaC5dgL0g8BiJSOp7Jj9dBbF2dmZCBNYmDZCnaeJ4H4pgZC5FxcxS2iGlHY' \
-     -F 'radddardev:boy=http://samples.ogp.me/431580313519917' \
-        'https://graph.facebook.com/me/radddardev:hanged_around'
+curl -F 'access_token=AAAC5JAD6lE4BADQqUHPYGa8iTguECttyvCtMZBN5jNiMCgXZAyLKDQRT7ie3xZBFFmsDrVeJtAJo4X7taGX9ciQ2Q9eFC4ZD' \
+     -F 'radddar=http://www.radddar.com/4f696467043bac0001000002' \
+        'https://graph.facebook.com/me/radddar:hang_around'
